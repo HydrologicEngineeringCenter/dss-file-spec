2cc113865058b9ce38d813bc1f331b95169dcdd7
Karl Tarbet <karl.l.tarbet@usace.army.mil>
2022-08-16 15:10:01 -0700

    ported DSS hash function from C

--- dss/DssHash.cs
+++ dss/DssHash.cs
@@ -8,24 +8,83 @@ namespace Hec.DssInternal
 {
    internal class DssHash
    {
-      string path;
+      string pathname;
       public DssHash(string path)
       {
-         this.path = path.ToUpper();
+         this.pathname = path.ToUpper();
 
       }
 
-      public long TableHash()
+      public long TableHash(int maxHash)
       {
+         //string expression = ".*(c:\\d{6})|.*/$";
+         //bool isCollection = path.IndexOf("/C:") >= 0;
+         var path = pathname;
+         int ibit=0;
+         double fraction = C.math.frexp((double)maxHash, ref ibit);
+         if (fraction == 0.50)
+         {
+            ibit--;
+         }
+         //  Find the number of characters that include full number groups
+         //  This rounds up to full number of 8 bit char to process when
+         //  you treat them in IBIT chunks
+         int nt = (((path.Length * 8) - 1) / ibit) + 1;
+         nt = (((nt * ibit) - 1) / 8) + 1;
 
-         return 0;
+         int it = 0;
+         int i2 = 0;
+         int ineed = ibit;
+         int ihave = 0;
+         int ibyte = 0;
+         while (true)
+         {
+            //  Get a group of bits
+            if (ihave > 0)
+            {
+               //  imove = min(ineed, ihave);
+               int imove = ineed;
+               if (ihave < imove) imove = ihave;
+               //  left shift
+               i2 = i2 << imove;
+               ineed -= imove;
+               ihave -= imove;
+               if (ineed <= 0)
+               {
+                  int itp = i2 / 256;
+                  i2 %= 256;
+                  //Use Exclusive OR to form value
+                  it ^= itp;
+                  ineed = ibit;
+               }
+            }
+           
+            //  Refill a character
+            if (ihave <= 0)
+            {
+               ++ibyte;
+               if (ibyte > nt)
+               {
+                  break;
+               }
+               //  Use the collection path, if it is one.
+               if(ibyte<=path.Length)
+                  i2 += path[ibyte - 1];
+               ihave = 8;
+            }
+         }
+         //  Compute final table hash
+         int tableHash = it % maxHash;
+         return tableHash;
+
+
       }
       public long PathHash()
       {
          long pathHash = 0;
-         for (int i = 0; i < path.Length; i++)
+         for (int i = 0; i < pathname.Length; i++)
          {
-            pathHash = (31 * pathHash) + path[i];
+            pathHash = (31 * pathHash) + (int)pathname[i];
          }
          //  Never allow a pathname bin hash to be zero
          if (pathHash == 0)
---
+++ test/HashTest.cs
@@ -0,0 +1,38 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Hec.DssInternal;
+namespace test
+{
+   [TestClass]
+   public class HashTest
+   {
+      const string path = "/RedFox RedFox/0/FLOW/01Jan2000/10Second/GATE/";
+      [TestMethod]
+      public void TestTableHash()
+      {
+         DssHash h = new DssHash(path);
+         int maxHash = 8192;
+         Assert.AreEqual(8182, h.TableHash(maxHash));
+      }
+      [TestMethod]
+      public void TestPathHash()
+      {
+         DssHash h = new DssHash(path);
+         Assert.AreEqual(3794281770676537607, h.PathHash());
+      }
+
+      [TestMethod]
+      public void Test_frexp()
+      {
+         int n = 0;
+        var mantissa= C.math.frexp((double)8192, ref n);
+         Assert.AreEqual(14, n);
+
+         double f = 123.45;
+         int i = 0;
+         //http://en.cppreference.com/w/c/numeric/math/frexp
+         mantissa = C.math.frexp(f, ref i);
+         Assert.AreEqual(0.964453d,mantissa,0.000001);
+         Assert.AreEqual(7, i);
+      }
+   }
+}
